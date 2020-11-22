ca5a8404316bbf8821a15e7f2ba5327580f37711
Ryan Geary <rgeary1@cs.binghamton.edu>
2020-11-22 12:57:01 -0500

    going to work on lab 12

--- submit/prj4-sol/ysim.c
+++ submit/prj4-sol/ysim.c
@@ -45,6 +45,21 @@ check_cc(const Y86 *y86, Byte op)
     ret = (get_sf(cc) ^ get_of(cc)) | get_zf(cc);
     break;
   //@TODO add other cases
+  case LT_COND:
+    ret = (get_sf(cc) ^ get_of(cc));
+    break;
+  case EQ_COND:
+    ret = get_zf(cc);
+    break;
+  case NE_COND:
+    ret = !get_zf(cc);
+    break;
+  case GE_COND:
+    ret = ~(get_sf(cc) ^ get_of(cc));
+    break;
+  case GT_COND:
+    ret = ~(get_sf(cc) ^ get_of(cc)) & !get_zf(cc);
+    break;
   default: {
     Address pc = read_pc_y86(y86);
     fatal("%08lx: bad condition code %d\n", pc, condition);
@@ -66,11 +81,17 @@ isLt0(Word word) {
 static void
 set_add_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
 {
-  //@TODO
+  if(read_status_y86(y86) != STATUS_AOK) return;
+	
+  Byte res = 0x0;
+  if(result == 0){ res &= 0xfb;}
+  if(((Word)(1) & (result>>(sizeof(Word)-1))) == 1){ res &= 0xfd;} 
+
+
 }
 
 /** Set condition codes for subtraction operation with operands opA, opB
- *  and result with result == opA - opB.
+ *  and result with result == opA - opB. 
  */
 static void
 set_sub_arith_cc(Y86 *y86, Word opA, Word opB, Word result)
@@ -91,6 +112,27 @@ op1(Y86 *y86, Byte op, Register regA, Register regB)
 {
   enum {ADDL_FN, SUBL_FN, ANDL_FN, XORL_FN };
   //@TODO
+  Word rA = read_register_y86(y86, regA);
+  Word rB = read_register_y86(y86, regB);
+  switch(get_nybble(op, 0)){
+	
+	case ADDL_FN :
+		set_add_arith_cc(y86, rA, rB, (long long)(rA) + (long long)(rB));
+	        break;
+        case SUBL_FN :
+		set_sub_arith_cc(y86, rA, rB, (long long)(rA) - (long long)(rB));
+	       	
+		break;
+ 	case ANDL_FN :
+		set_logic_op_cc(y86, rA & rB);
+		break;
+	case XORL_FN :
+		set_logic_op_cc(y86, rA ^ rB);
+		break;
+	default :
+		write_status_y86(y86, STATUS_INS);
+		break;		
+  }
 }
 
 /*********************** Single Instruction Step ***********************/
@@ -107,5 +149,72 @@ typedef enum {
 void
 step_ysim(Y86 *y86)
 {
-  //@TODO
+  if(read_status_y86(y86) != STATUS_AOK) return;
+
+  Address pc = read_pc_y86(y86);
+  Address ad = read_memory_byte_y86(y86, pc);
+  
+  
+  switch(get_nybble(ad, 1)){
+	
+	  case HALT_CODE : ;
+		  write_status_y86(y86, STATUS_HLT);
+		  break;
+	  case NOP_CODE : ;
+		  write_pc_y86(y86, pc + sizeof(Address)/4);
+		  break;
+	  case IRMOVQ_CODE : ;
+		Word temp = read_memory_word_y86(y86, pc + 1 + sizeof(Byte));
+		write_register_y86(y86, get_nybble(read_memory_byte_y86(y86, pc + 1), 0), temp);
+		write_pc_y86(y86, pc + 1 + sizeof(Byte) + sizeof(Word));
+		break;
+	  case CALL_CODE: ;
+		Word code = read_memory_word_y86(y86, read_pc_y86(y86) + 1);
+		write_register_y86(y86, REG_RSP , read_register_y86(y86, REG_RSP) - sizeof(Address));
+		write_memory_word_y86(y86, read_register_y86(y86, REG_RSP), pc + 1 + sizeof(Address));
+
+		write_pc_y86(y86, code);
+		
+		break;
+	  case RET_CODE : ;
+		write_pc_y86(y86, read_memory_word_y86(y86, read_register_y86(y86, REG_RSP)));
+		write_register_y86(y86, REG_RSP, read_register_y86(y86, (REG_RSP))+sizeof(Address));
+		break;
+	  case MRMOVQ_CODE: ;
+		Byte reg = read_memory_byte_y86(y86, pc + sizeof(Byte));
+		Word offs = read_memory_word_y86(y86, pc + 2*sizeof(Byte));
+		Register srcReg = get_nybble(reg, 0);
+		Register destReg = get_nybble(reg, 1);
+		write_register_y86(y86, destReg, read_memory_word_y86(y86, offs + read_register_y86(y86, srcReg)));
+		write_pc_y86(y86,  pc + 1 + sizeof(Byte) + sizeof(Word));
+		break;
+	  case RMMOVQ_CODE: ;
+		Byte regs = read_memory_byte_y86(y86, pc + sizeof(Byte));
+		Word offset = read_memory_word_y86(y86, pc + 2*sizeof(Byte));
+		Register src = get_nybble(regs, 1);
+		Register dest = get_nybble(regs, 0);
+		Address destination = read_register_y86(y86, dest) + offset;
+		Word source = read_register_y86(y86, src);
+		write_memory_word_y86(y86, destination, source);
+		write_pc_y86(y86,  pc + 1 + sizeof(Byte) + sizeof(Word));
+	        break;	
+	  case CMOVxx_CODE: ;
+		if(check_cc(y86, read_memory_byte_y86(y86, pc))){
+			Byte regc = read_memory_byte_y86(y86, pc + 1);
+			write_register_y86(y86, get_nybble(regc, 0), read_register_y86(y86, get_nybble(regc, 1)));
+		}	
+		write_pc_y86(y86, pc + 1 + sizeof(Byte));
+		break;	
+	  default : ;
+		write_status_y86(y86, STATUS_INS);
+		break;
+  }
+ /* 
+ 	  printf("REG_RSP: %lu \n", read_register_y86(y86, REG_RSP));	  
+	  printf("REG_RAX: %lu \n", read_register_y86(y86, REG_RAX));
+	  printf("REG_RBX: %lu \n", read_register_y86(y86, REG_RBX));
+	  printf("REG_RSI: %lu \n", read_register_y86(y86, REG_RSI));
+	  printf("------------- \n");
+	  */
+
 }
